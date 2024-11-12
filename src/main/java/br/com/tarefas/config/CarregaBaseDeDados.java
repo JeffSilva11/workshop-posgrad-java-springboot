b289bce77d80e05a31dcfaa316e777573f652d17
Jefferson Silva <fj_adm@hotmail.com>
2024-11-12 20:41:39 -0300

    UN03 - 09 - Alteradas: Tarefa/TarefaCategoria/TarefaController/pm.xml - Criadas: messages.properties/InternacionalizacaoConfig/messages_en_US.properties/messages_pt_BR.properties/TarefaService/TarefaServiceIntegrationTest/TarefaStatusException/ TarefaServiceTest / TarefaResponse/TarefaCategoriaResponse/MapperConfi/TarefaRequest - Anotações BeanValidation, Testes unitários e Add dependency modelmapper

--- src/main/java/br/com/tarefas/config/CarregaBaseDeDados.java
+++ src/main/java/br/com/tarefas/config/CarregaBaseDeDados.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Profile;
 
 import br.com.tarefas.model.Tarefa;
 import br.com.tarefas.model.TarefaCategoria;
@@ -17,7 +17,7 @@ import br.com.tarefas.repository.TarefaRepository;
 import br.com.tarefas.repository.UsuarioRepository;
 
 @Configuration
-//@Profile("dev")
+@Profile("dev")
 public class CarregaBaseDeDados {
 	
 	@Autowired
@@ -34,11 +34,13 @@ public class CarregaBaseDeDados {
 		return args -> {
 			Usuario usuario = new Usuario();
 			usuario.setNome("Admin");
-			usuario.setSenha("123456");			
+			usuario.setSenha("123456");
+			
 			usuarioRepository.save(usuario);
 			
 			TarefaCategoria categoria = new TarefaCategoria();
-			categoria.setNome("Estudos");			
+			categoria.setNome("Estudos");
+			
 			categoriaRepository.save(categoria);
 			
 			Tarefa tarefa = new Tarefa();
@@ -47,16 +49,17 @@ public class CarregaBaseDeDados {
 			tarefa.setStatus(TarefaStatus.ABERTO);
 			tarefa.setVisivel(true);
 			tarefa.setCategoria(categoria);
-			tarefa.setUsuario(usuario);			
-			tarefaRepository.save(tarefa);
+			tarefa.setUsuario(usuario);
 			
+			tarefaRepository.save(tarefa);
 			Tarefa tarefa2 = new Tarefa();
 			tarefa2.setDescricao("Aprender Spring Data JPA");
 			tarefa2.setDataEntrega(LocalDate.now().plusDays(1));
 			tarefa2.setStatus(TarefaStatus.ABERTO);
 			tarefa2.setVisivel(true);
 			tarefa2.setCategoria(categoria);
-			tarefa2.setUsuario(usuario);			
+			tarefa2.setUsuario(usuario);
+			
 			tarefaRepository.save(tarefa2);
 			
 		};
